c80241e2a5c2cec7b884573df2d4da1394784720
Soumik Ranjan Dasgupta <dasguptaarjo@gmail.com>
2017-12-30 21:12:51 +0530

    Changed comment format

--- audiocrop.py
+++ audiocrop.py
@@ -1,17 +1,22 @@
 from pydub import AudioSegment
-audio_track=AudioSegment.from_file("sample_track.mp3")					#open audio track file
+#open audio track file
+audio_track=AudioSegment.from_file("sample_track.mp3")					
 
-song_length,song_name=[],[]												#store details of song (could be done in a single list)
+song_length,song_name=[],[]												
+#store details of song (could be done in a single list)
 for i in range(int(input("Enter number of songs, and then duration(in seconds) and name of each song \n"))):
 	song_length.append(int(input())*1000)		
 	song_name.append(str(input()+".mp3"))
 
-start_song=song_length[0]												#initialise counter for durations
+#initialise counter for durations
+start_song=song_length[0]												
 for j in range(len(song_length)):
-	if j==0:															#for first song
-		song=audio_track[:song_length[0]]								#crop audio track
+	#for first song
+	if j==0:															
+		#crop audio track
+		song=audio_track[:song_length[0]]								
 	else:
 		song=audio_track[start_song:start_song+song_length[j]]
-		start_song+=song_length[j]										#update counter
-	song.export(song_name[j])											#save and close file with user-entered details (default:MP3 format)
-
+		start_song+=song_length[j]	
+	#save and close file with user-entered details (default:MP3 format)
+	song.export(song_name[j])											
